d98e6c793765d163aa9b974590c99cd9e772c8a0
Anvit Patil <45007007+anvitpatil@users.noreply.github.com>
2021-08-24 15:39:36 +0530

    Update q3.c

--- assign_one/q3.c
+++ assign_one/q3.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    int n,count;
-    printf("Enter the number of elements:\n");
-    scanf("%d",&n);
+    int n = 20,count;
+    printf("Number of elements: %d\n",n);
     int *arr = (int*)malloc(n*sizeof(int));
     printf("Enter the numbers\n");
     for(int i=0;i<n;i++){
